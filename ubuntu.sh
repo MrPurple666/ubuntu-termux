041bc951d1d622262c745a3d04d6200cf6eb2172
Mr_Purple_666 <antoniosacramento666usa@gmail.com>
2022-01-25 15:17:38 -0300

    update for compatibility

--- ubuntu.sh
+++ ubuntu.sh
@@ -69,7 +69,7 @@ cd \$(dirname \$0)
 unset LD_PRELOAD
 command="proot"
 ## uncomment following line if you are having FATAL: kernel too old message.
-#command+=" -k 4.14.81"
+command+=" -k 5.17-rc1"
 command+=" --link2symlink"
 command+=" -0"
 command+=" -r $directory"
