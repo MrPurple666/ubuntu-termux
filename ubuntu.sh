82e2bdf9ed2af74ba776d22f443b6301626e39bc
Mr_Purple_666 <antoniosacramento666usa@gmail.com>
2022-02-21 13:55:09 -0300

    Fix kernel

--- ubuntu.sh
+++ ubuntu.sh
@@ -68,8 +68,7 @@ cd \$(dirname \$0)
 ## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
-## uncomment following line if you are having FATAL: kernel too old message.
-#command+=" -k 4.14.81"
+command+=" -k 4.14.266-LTS"
 command+=" --link2symlink"
 command+=" -0"
 command+=" -r $directory"
