b8a506b2a96aef58932e1cc0b43f3e80b71d95bd
MrPurple666 <antoniosacramento666usa@gmail.com>
2022-02-26 01:07:27 -0300

    init scripts branch
    Thanks a lot @tuanpham-dev for the scripts
    removed unnecessary parts of vnc

---
+++ xfce.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Get the necessary components
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:xubuntu-dev/staging -y
+sudo apt update
+sudo apt install udisks2 -y
+echo "" > /var/lib/dpkg/info/udisks2.postinst
+sudo dpkg --configure -a
+sudo apt-mark hold udisks2
+sudo apt install keyboard-configuration -y
+sudo apt install tzdata -y
+sudo apt install sudo wget nano inetutils-tools dialog -y
+sudo apt install xfce4 xfce4-goodies xfce4-terminal tigervnc-standalone-server tigervnc-common dbus-x11 ffmpeg language-pack-en --no-install-recommends -y
+sudo apt --fix-broken install
+sudo apt clean
+
+mkdir -p ~/.vnc
+
+echo " "
+
+echo "Running browser patch"
+wget https://raw.githubusercontent.com/tuanpham-dev/termux-ubuntu/master/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
+sudo ./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
