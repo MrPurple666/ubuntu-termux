9703acb7b458ce9987ef0da81b4276b55624b197
Mr_Purple_666 <antoniosacramento666usa@gmail.com>
2022-07-19 15:03:51 -0300

    Remove sudo mentions bcz u are root

--- xfce.sh
+++ xfce.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Get the necessary components
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:xubuntu-dev/staging -y
-sudo apt update
-sudo apt install udisks2 -y
+apt install software-properties-common -y
+add-apt-repository ppa:xubuntu-dev/staging -y
+apt update
+apt install udisks2 -y
 echo "" > /var/lib/dpkg/info/udisks2.postinst
-sudo dpkg --configure -a
-sudo apt-mark hold udisks2
-sudo apt install keyboard-configuration -y
-sudo apt install tzdata -y
-sudo apt install sudo wget nano inetutils-tools dialog -y
-sudo apt install xfce4 xfce4-goodies xfce4-terminal dbus-x11 ffmpeg language-pack-en --no-install-recommends -y
-sudo apt install tightvncserver -y
-sudo apt --fix-broken install
-sudo apt clean
+dpkg --configure -a
+apt-mark hold udisks2
+apt install keyboard-configuration -y
+apt install tzdata -y
+apt install sudo wget nano inetutils-tools dialog -y
+apt install xfce4 xfce4-goodies xfce4-terminal dbus-x11 ffmpeg language-pack-en --no-install-recommends -y
+apt install tightvncserver -y
+apt --fix-broken install
+apt clean
 
 mkdir -p ~/.vnc
 
@@ -21,7 +21,7 @@ echo " "
 
 echo "Running browser patch"
 wget https://raw.githubusercontent.com/MrPurple666/ubuntu-termux/scripts/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
-sudo ./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
+ ./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
 
 echo "Running QT install"
 chmod +x qt.sh
